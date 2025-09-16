be0b868a154207a5586269ea053b886c99504aae
Jayromulus <jerbear2@gmail.com>
2025-09-16 16:07:34 -0400

    added sigil information

---
+++ assets/sigilData.js
@@ -0,0 +1,451 @@
+module.exports = {
+  //? General
+  "Solidify Defense": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "Gain an additional 2 **DEF** when adding **DEF** next time"
+  },
+  "Regulate Breathing": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 4,
+    "Effect": "Consume 1 **Qi** at the end of this turn: **HP** and **Max HP** + 6"
+  },
+  "Exorcise Evil": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 4,
+    "Effect": "Repeat 2 times: randomly reduce 1 stack of **Debuff**"
+  },
+  "Ward Off Evil": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 1,
+    "HP Modification": 4,
+    "Effect": "Gain 2 **Hexproof**"
+  },
+  "Draw Dragon": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 1,
+    // "HP Modification": 0,
+    "Effect": "When you play a card that can be **Upgraded** next time, **Upgrade** it (restored after the battle)"
+  },
+  "HP": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "**HP** + 2"
+  },
+  "Expand Vitality": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 4,
+    "Effect": "**Max HP** + 14"
+  },
+  "Surge of Divine Power": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    // "HP Modification": 0,
+    "Effect": "Gain an additional 1 stack when gaining **Increase ATK** next time"
+  },
+  "Bear": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 2,
+    "HP Modification": 16,
+    "Passive Effect": "You can add 1 more **Continuous** or **Consumption** card to your deck"
+  },
+  "Disaster": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 1,
+    // "HP Modification": 0,
+    "Effect": "When the opponent plays a card that can be **Downgraded** next time, **Downgrade** it (restored after the battle)"
+  },
+  //? Cloud Spirit Sword Sect
+  "Cloud Rise": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Passive Effect": "Treated as having played **Cloud Sword** once at the start of the battle"
+  },
+  "Clouds Gather and Stars Converge": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Passive Effect": "After playing **Cloud Sword - Touch Sky** or **Cloud Sword - Pierce the Star**, all your cards are treated as **Cloud Sword** until the end of battle"
+  },
+  "Madness": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "The next card played is treated as **Unrestrained Sword**"
+  },
+  "Hundred Birds": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 1,
+    // "HP Modification": 0,
+    "Passive Effect": "Reduce the **Qi** cost of **Spirit Sword** cards by 1"
+  },
+  "Spirit and Frenzy": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 2,
+    "HP Modification": 10,
+    "Passive Effect": "Gain 1 **Qi** each time you play **Unrestrained Sword**; **Spirit Sword** played also counts as **Unrestrained Sword** played"
+  },
+  "Spirit Slash": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Passive Effect": "Deal 2 more **ATK** in the next **Spirit Sword** attack"
+  },
+  "Harm the Body": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 4,
+    "Passive Effect": "Reduce the opponent's **HP** by 2 in the next attack, and it will definitely be treated as **Injured** the opponent"
+  },
+  "Necessity": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 2,
+    "HP Modification": 8,
+    "Passive Effect": "When gaining **Ignore DEF** via **Cloud Sword - Necessity**, also gain the same number of **Increase ATK**"
+  },
+  "Soft Heart": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 3,
+    "HP Modification": 8,
+    "Effect": "After playing **Cloud Sword - Softheart** next time, increase **Max HP** by 20 and trigger **Chase**"
+  },
+  "Cloud Sea Wandering Dragon": {
+    "sect": "Cloud Spirit Sword Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 3,
+    "HP Modification": 12,
+    "Passive Effect": "After playing the **Cloud Sword** in the first slot of the deck, trigger **Chase** if you **Injured** the opponent\nAll your **Cloud Sword - Dragon Roam** gain **Chase**"
+  },
+  //? Heptastar Pavillion
+  "Shift Stars": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 1,
+    "Effect": "Gain an additional grid as **Star Point** when playing **Shifting Stars** or **Starry Moon** next time"
+  },
+  "Gather Star Power": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "Gain an additional 1 **Star Power** after playing **Dotted Around** or **Astral Fleche** next time"
+  },
+  "Transform Star Spirit": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 6,
+    "Passive Effect": "If **Qi** is insufficient when playing a card, consume 1 **Star Point** to gain 2 **Qi**"
+  },
+  "Spring Return": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 2,
+    "HP Modification": 2,
+    "Effect": "When playing **Revitalized** or the card in the 8th slot of your deck next time, increase **HP** and **Max HP** by 3 for every 1 remaining **Qi**"
+  },
+  "Continue Heavenly Fate": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 2,
+    // "HP Modification": 0,
+    "Passive Effect": "When your **HP** drops to <= 0 for the first time in battle, increase **HP** and **Max HP** by 3 for each **Hexagram** you have, then continue fighting if **HP** > 0"
+  },
+  "Hexagram Fall": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    // "HP Modification": 0,
+    "Effect": "Deal 4 **DMG** to the opponent when adding **Hexagram** next time"
+  },
+  "Dark Star Soul Injury": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Passive Effect": "Effects that increase **Star Power** are changed to apply the same amount of **Internal Injury** to the opponent"
+  },
+  "Thunder Break": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 3,
+    "Effect": "When playing a **Thunder** card next time, apply 1 **Wound** on the opponent, and the ATK gains **Smash DEF**"
+  },
+  "Sacrifice to Thunder": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 3,
+    // "HP Modification": 0,
+    "Effect": "When playing a **Thunder** card next time, reduce your **HP** by 2 and gain 4 **Hexagram**"
+  },
+  "Aggravate Internal Injury": {
+    "sect": "Heptastar Pavillion",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 3,
+    "HP Modification": 6,
+    "Effect": "Double the opponent's **Internal Injury**\nIf the opponent has no **Internal Injury**, instead apply 6 **Internal Injury**"
+  },
+  //? Five Elements Alliance
+  "Sharp Thrust": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 1,
+    "Effect": "Reduce the opponent's **HP** by an additional 3 when **Penetrate** is triggered next time"
+  },
+  "Five Elements Spirit Spin": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Passive Effect": "Every time **Activate** is repeated on the activated **Five Elements**, deal 2 **DMG** to the opponent"
+  },
+  "Earth's Firmness": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "Gain 6 **DEF** when playing an **Earth Spirit** card next time"
+  },
+  "Water Spirit": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "Gain 2 **Qi** when playing a **Water Spirit** card next time"
+  },
+  "Heavenly Marrow": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 3,
+    "HP Modification": 5,
+    "Passive Effect": "The **Continuous** count of **Five Elements Heavenly Marrow Rhythm** is doubled, and it can also take effect when the deck contains only 2 types of **Five Elements**"
+  },
+  "Hot Sand": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "Deal 2 **DMG** to the opponent when playing **Earth Spirit** or **Fire Spirit** card next time"
+  },
+  "Young Seedling": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 2,
+    "HP Modification": 6,
+    "Effect": "Gain 1 stacks of **Increase ATK** after playing the Foundation Phase **Wood Spirit** card next time"
+  },
+  "Metal's Sharpness": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 4,
+    "Effect": "Gain 2 **Penetrate** when playing a **Metal Spirit** card next time"
+  },
+  "Blend Cosmos": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 1,
+    // "HP Modification": 0,
+    "Passive Effect": "When counting the number of different **Five Elements** in your deck, it is treated as an additional +1"
+  },
+  "Refine Cauldron": {
+    "sect": "Five Elements Alliance",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 1,
+    "HP Modification": 9,
+    "Effect": "Permanently turn the **Metal Spirit** card on the far right of your hand into a Level 1 **Metal Spirit** card of the next higher phase (maximum up to Incarnation Phase)"
+  },
+  //? Duan Xuan Sect
+  "Resist Xuan": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "Gain 3 **DEF** when receiving a **Debuff** next time"
+  },
+  "Break Free": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 3,
+    "Effect": "At the end of this turn, reduce **Weakened**, **Flaw**, and **Entangled** by an additional 2 stacks each"
+  },
+  "Crash Fist Power": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 3,
+    "Passive Effect": "Your **Crash Fist** deals 1 more **ATK**"
+  },
+  "Temper Nether Seal": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 1,
+    "HP Modification": 3,
+    "Effect": "Gain 1 **Physique** for every 2 stacks of **Debuff** you have (maximum gain 4)"
+  },
+  "Scarlet Blood Rage": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 3,
+    "HP Modification": 12,
+    "Passive Effect": "Each time you play a **HP** cost card, deal **DMG** equal to the **HP** cost of the opponent"
+  },
+  "Fist Training": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "Gain 1 **Physique** after playing **Exercise Fist** next time"
+  },
+  "Resurrection": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Effect": "When playing **HP** cost cards for the next 1 times, gain **HP** equal to the amount of **HP** cost"
+  },
+  "Break Limit": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 1,
+    "HP Modification": 4,
+    "Effect": "The maximum DEF increase limit of **Detect-Horse Palms** is increased by 4, and the maximum ATK increase limit of **Windward Palms** is increased by 2"
+  },
+  "Lost Track": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 1,
+    "HP Modification": 2,
+    "Passive Effect": "The continuous effect of **Elusive Footwork** can also be triggered once per turn during the opponent's turn"
+  },
+  "Blood Shadow Escape": {
+    "sect": "Duan Xuan Sect",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 2,
+    "HP Modification": 4,
+    "Effect": "Gain 1 **Agility** for every 1 **HP** in cost when playing the next **HP Cost** card"
+  },
+}
+
+
+/*
+  "": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Meditation",
+    "Sigil Value": 0,
+    "HP Modification": 0,
+    "Effect": ""
+  },
+  "": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Foundation",
+    "Sigil Value": 0,
+    "HP Modification": 0,
+    "Effect": ""
+  },
+  "": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Virtuoso",
+    "Sigil Value": 0,
+    "HP Modification": 0,
+    "Effect": ""
+  },
+  "": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Immortality",
+    "Sigil Value": 0,
+    "HP Modification": 0,
+    "Effect": ""
+  },
+  "": {
+    "sect": "General",
+    "image": "https://i.imgur.com/AfFp7pu.png",
+    "Phase": "Incarnation",
+    "Sigil Value": 0,
+    "HP Modification": 0,
+    "Effect": ""
+  },
+*/
